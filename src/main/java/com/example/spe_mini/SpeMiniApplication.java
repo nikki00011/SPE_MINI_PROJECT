5248073ef2f020e8a16fc4735f25bc9ba825d4eb
nikki00011 <nikita.gupta@iiitb.ac.in>
2024-02-24 22:02:17 +0530

    made changes in the file

--- src/main/java/com/example/spe_mini/SpeMiniApplication.java
+++ src/main/java/com/example/spe_mini/SpeMiniApplication.java
@@ -1,14 +1,12 @@
 package com.example.spe_mini;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 @SpringBootApplication
-@RestController
 public class SpeMiniApplication {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
